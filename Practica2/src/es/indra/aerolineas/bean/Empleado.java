cd2d908d7d82d8525fff5cdd96a3796fa949a0f9
Carlos Alcalde <carlos.alcalde.benitez@gmail.com>
2018-11-06 15:28:41 +0100

    first commit

---
+++ Practica2/src/es/indra/aerolineas/bean/Empleado.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package es.indra.aerolineas.bean;
+
+/**
+ * @author aula6
+ *
+ */
+public class Empleado extends Persona{
+
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
