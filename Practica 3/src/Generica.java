9927c6442dc08b582160844646bd90340c63c5a3
quiquepi95 <quiquepin01@gmail.com>
2018-11-07 11:54:01 +0100

    Añadido Genericos

---
+++ Practica 3/src/Generica.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author aula8
+ *
+ */
+public class Generica<T> {
+	
+	private T t;
+
+	public T getT() {
+		return t;
+	}
+	public void setT(T t) {
+		this.t = t;
+	}
+	
+}
