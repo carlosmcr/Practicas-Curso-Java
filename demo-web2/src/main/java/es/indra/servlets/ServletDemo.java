e8fa994cec3e3262d199c83375be8f5455ae35f0
Carlosmcr <carlos-mcr@hotmail.com>
2018-11-13 11:21:27 +0100

    Servlet

---
+++ demo-web2/src/main/java/es/indra/servlets/ServletDemo.java
@@ -0,0 +1,34 @@
+package es.indra.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletDemo
+ */
+@WebServlet("/ServletDemo")
+public class ServletDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ServletDemo() {
+        super();
+    }
+    
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	response.sendRedirect("index.jsp");
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
